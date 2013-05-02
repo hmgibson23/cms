9487c5e4ebad7ef67436a5fd035193766dc3bc2c
Hugo Gibson <hugo.gibson@yahoo.co.uk>
2013-05-02 19:14:17 +0100

    Wrapped app

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-
 //little jquery extension to serialize objects
 $.fn.serializeObject = function() {
     var o = {};
@@ -16,178 +15,195 @@ $.fn.serializeObject = function() {
     return o;
 };
 
-var Vehicle = Backbone.Model.extend({
-
-    //maps directly to mongo _id
-    idAttribute: "_id",
-
-    defaults: {
-        image : "http://placehold.it/320x200",
-        manufacturer: "",
-        registration: "",
-        color: "",
-        mileage: "",
-        description: "",
-        list_price: null,
-        sold: false,
-        date_registered: null,
-        date_sold: null
-    }
-});
-
-//VEHICLE COLLECTION
-var VehicleList = Backbone.Collection.extend({
-    model: Vehicle,
-    url: '/vehicles'
-});
 
 
-//init vehicle list
-var Vehicles = new VehicleList();
+var CMS = (function(Backbone, $) {
+    var Vehicle = Backbone.Model.extend({
+
+        //maps directly to mongo _id
+        idAttribute: "_id",
+
+        defaults: {
+            image: "http://placehold.it/320x200",
+            manufacturer: "",
+            registration: "",
+            color: "",
+            mileage: "",
+            description: "",
+            list_price: null,
+            sold: false,
+            date_registered: null,
+            date_sold: null
+        }
+    });
+
+    //VEHICLE COLLECTION
+    var VehicleList = Backbone.Collection.extend({
+        model: Vehicle,
+        url: '/vehicles'
+    });
 
 
-/*=============================
+    //init vehicle list
+    var Vehicles = new VehicleList();
+
+
+    /*=============================
 =            VIEWS            =
 =============================*/
-var VehicleView = Backbone.View.extend({
+    var VehicleView = Backbone.View.extend({
 
-    tagName: 'li',
-    className: 'vehicle-li',
+        tagName: 'li',
+        className: 'vehicle-li',
 
-    events: {
-        "click button.destroy" : "clear",
-        "click button.edit" : "edit",
-        "submit .edit-form": "update",
-        "click .cancel": "cancel",
-        "click .sold": "sold"
-    },
+        events: {
+            "click button.destroy": "clear",
+            "click button.edit": "edit",
+            "submit .edit-form": "update",
+            "click .cancel": "cancel",
+            "click .sold": "sold"
+        },
 
-    template: JST['vehicle/li'],
+        template: JST['vehicle/li'],
 
-    initialize: function() {
-      this.listenTo(this.model, 'change', this.render);
-      this.listenTo(this.model, 'destroy', 'destroy', this.remove);
-  },
+        initialize: function() {
+            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.model, 'destroy', 'destroy', this.remove);
+        },
 
-  render: function() {
-    var dict = this.model.toJSON();
-    this.$el.html(this.template(dict));
-    return this;
-},
+        render: function() {
+            var dict = this.model.toJSON();
+            this.$el.html(this.template(dict));
+            return this;
+        },
 
-cancel : function() {
-    this.$('.edit-form').hide();
-},
+        cancel: function() {
+            this.$('.edit-form').hide();
+        },
 
-clear: function() {
-    this.model.destroy();
-},
+        clear: function() {
+            this.model.destroy();
+        },
 
-edit: function() {
-    this.$('.edit-form').slideDown();
-},
+        edit: function() {
+            this.$('.edit-form').slideDown();
+        },
 
-update: function(event) {
-    event.preventDefault();
-    var form = this.$('.edit-form').serializeObject();
-    this.model.set(form);
-    this.model.save();
-},
+        update: function(event) {
+            event.preventDefault();
+            var form = this.$('.edit-form').serializeObject();
+            this.model.set(form);
+            this.model.save();
+        },
 
-sold: function() {
-    this.model.set({'sold': true});
-    this.model.set({'date_sold': new Date()});
-    this.model.save();
-}
+        sold: function() {
+            this.model.set({
+                'sold': true
+            });
+            this.model.set({
+                'date_sold': new Date()
+            });
+            this.model.save();
+        }
 
-});
-
-var VehiclesView = Backbone.View.extend({
-    el: $('#vehicles'),
-
-    template: JST['vehicles/main'],
-
-    events: {
-      "click #new-vehicle-click":  "showNewVehicleForm",
-      "submit #new-vehicle-form":  "createNewVehicle",
-      "click .cancel": "hideForm"
-  },
-
-  initialize: function() {
-    this.listenTo(Vehicles, 'add', this.addOne);
-    this.listenTo(Vehicles, 'reset', this.addAll);
-    Vehicles.fetch();
-},
-
-render: function() {
-    this.$el.append(this.template());
-    return this;
-},
-
-showNewVehicleForm: function() {
-    this.$('#new-vehicle-form').slideDown('slow');
-},
-
-createNewVehicle: function(form) {
-    form.preventDefault();
-    var veh = new Vehicle();
-    veh.set({
-        manufacturer : this.$('input[name=manu]').val(),
-        registration : this.$('input[name=vehicle_reg]').val(),
-        mileage : this.$('input[name=mileage]').val(),
-        list_price : this.$('input[name=list_price]').val(),
-        color : this.$('input[name=color]').val(),
-        description : this.$('input[name=description]').val()
     });
-    Vehicles.create(veh);
-    this.$('#new-vehicle-form').slideUp('slow');
-},
 
-addOne: function(vehicle) {
-    var view = new VehicleView({model: vehicle});
-    this.$('#vehicle-list').append(view.render().el);
-},
+    var VehiclesView = Backbone.View.extend({
+        el: $('#vehicles'),
 
-addAll: function() {
-  Vehicles.each(this.addOne, this);
-},
+        template: JST['vehicles/main'],
 
-hideForm: function() {
-    this.$('#new-vehicle-form').slideUp('slow');
-}
-});
+        events: {
+            "click #new-vehicle-click": "showNewVehicleForm",
+            "submit #new-vehicle-form": "createNewVehicle",
+            "click .cancel": "hideForm"
+        },
+
+        initialize: function() {
+            this.listenTo(Vehicles, 'add', this.addOne);
+            this.listenTo(Vehicles, 'reset', this.addAll);
+            Vehicles.fetch();
+        },
+
+        render: function() {
+            this.$el.append(this.template());
+            return this;
+        },
+
+        showNewVehicleForm: function() {
+            this.$('#new-vehicle-form').slideDown('slow');
+        },
+
+        createNewVehicle: function(form) {
+            form.preventDefault();
+            var veh = new Vehicle();
+            veh.set({
+                manufacturer: this.$('input[name=manu]').val(),
+                registration: this.$('input[name=vehicle_reg]').val(),
+                mileage: this.$('input[name=mileage]').val(),
+                list_price: this.$('input[name=list_price]').val(),
+                color: this.$('input[name=color]').val(),
+                description: this.$('input[name=description]').val()
+            });
+            Vehicles.create(veh);
+            this.$('#new-vehicle-form').slideUp('slow');
+        },
+
+        addOne: function(vehicle) {
+            var view = new VehicleView({
+                model: vehicle
+            });
+            this.$('#vehicle-list').append(view.render().el);
+        },
+
+        addAll: function() {
+            Vehicles.each(this.addOne, this);
+        },
+
+        hideForm: function() {
+            this.$('#new-vehicle-form').slideUp('slow');
+        }
+    });
 
 
 
-/**
-*
-* Main Application View
-*
-**/
-var AppView = Backbone.View.extend({
+    /**
+     *
+     * Main Application View
+     *
+     **/
+    var AppView = Backbone.View.extend({
 
-    el: $('body'),
+        el: $('body'),
 
-    events: {
-        'click a.vehicles' : 'loadVehicles',
-        'click a.parts' : 'loadParts',
-        'click a.accounting' : 'loadAccounting'
-    },
+        events: {
+            'click a.vehicles': 'loadVehicles',
+            'click a.parts': 'loadParts',
+            'click a.accounting': 'loadAccounting'
+        },
 
-    loadVehicles: function() {
-        this.$('#vehicles').remove();
-        $('#right-panel').append("<div id='vehicles'></div>");
-        this.vehicles = new VehiclesView();
-    },
+        loadVehicles: function() {
+            this.$('#vehicles').remove();
+            $('#right-panel').append("<div id='vehicles'></div>");
+            this.vehicles = new VehiclesView();
+        },
 
-    initialize: function() {
-        this.vehicles = new VehiclesView();
-        this.vehicles.render();
+        initialize: function() {
+            this.vehicles = new VehiclesView();
+            this.vehicles.render();
+        }
+
+    });
+
+    return {
+        init :function() {
+        var view = new AppView();
     }
+}
 
-});
+})(Backbone, jQuery);
 
 //init app    
-$(function(){
-    var view = new AppView();
+$(function() {
+    CMS.init();
 });
